5de72e207241cc58640531edf9c82c787a37d5ee
Andres <andres.montenegro1@gmail.com>
2017-12-12 23:56:49 -0500

    Initial commit

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"SortArray","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"SortArray","l":"SortArray()"}]
---
+++ src/SortArray.java
@@ -0,0 +1,9 @@
+public class SortArray {
+    public static void main(String [] args){
+
+        String [] list = {"13","6","1","7","4","6","2","8","1","2"};
+        for (int i=0; i<list.length; i++) {
+            System.out.println(list[i]);
+        }
+    }
+}
